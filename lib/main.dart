0fedde649346766ed593cbda80edd86890578140
Kyrel Jerome <kyrel2007@gmail.com>
2019-01-13 05:59:30 -0500

    Changed default theme brightness to dark

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Deer Food',
-      theme: ThemeData(
+      theme:
+      ThemeData(
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark
       ),
       home: FoodDisplayRoute(title: 'University of Toronto Foods'),
     );
