effd22753f1fb3123f764e3f4f5d6a1cc939b79b
Kyrel Jerome <kyrel2007@gmail.com>
2019-01-12 21:10:50 -0500

    Added classes Hours, Stores

--- lib/FoodDisplayRoute.dart
+++ lib/FoodDisplayRoute.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'Objects/Store.dart';
+import 'Objects/Hours.dart';
 
 class FoodDisplayRoute extends StatefulWidget {
   FoodDisplayRoute({Key key, this.title}) : super(key: key);
+  final double margin = 8;
   final String title;
   @override
   _FoodDisplayRouteState createState() => _FoodDisplayRouteState();
 }
 
 class _FoodDisplayRouteState extends State<FoodDisplayRoute> {
-  //List<Store> stores;
-  void initState() { 
+  List<Store> stores;
+  void initState() {
     super.initState();
-    
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +23,42 @@ class _FoodDisplayRouteState extends State<FoodDisplayRoute> {
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: ListView(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(widget.margin),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: buildFiltersList(),
+            ),
+          ),
+          ListView(),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: buildBottomNavigationBar(),
       ),
     );
   }
 
+  List<Widget> buildFiltersList() {
+    return [
+      Chip(
+        label: Text('nah'),
+      ),
+      Chip(
+        label: Text('nah'),
+      )
+    ];
+  }
   List<BottomNavigationBarItem> buildBottomNavigationBar() {
     List<BottomNavigationBarItem> items = List();
-    items.add(BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("UTM")));
-    items.add(BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("UTSG")));
-    items.add(BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("UTSC")));
+    items.add(
+        BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("UTM")));
+    items.add(
+        BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("UTSG")));
+    items.add(
+        BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("UTSC")));
     //TODO: Replace with campus logos?
     return items;
   }
---
+++ lib/Objects/Store.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'Hours.dart';
+class Store{
+  String name;
+  Image Logo;
+  String website;
+  String location;
+  Hours hours;
+}
