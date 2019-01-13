7d20cc0eaad8ea0b2ce29720cd67bd0199985fe8
Kyrel Jerome <kyrel2007@gmail.com>
2019-01-13 12:46:37 -0500

    Added Json parsing to Store.dart

--- lib/Objects/Hours.dart
+++ lib/Objects/Hours.dart
@@ -1,3 +1,7 @@
 class  Hours{
   Map<String, Map<String,int>> hours;
+  factory Hours.fromJson(Map<String, dynamic> parsedJson){
+    return Hours
+  }
+  getHoursDate()
 }
--- lib/Objects/Store.dart
+++ lib/Objects/Store.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 import 'Hours.dart';
-class Store{
-  String _id;
-  String _buildingID;
 
+class Store {
+  String id;
+  String buildingID;
   Image logo;
-  String _name;
-  String _shortName;
-  String _campus;
-  
-  String _website;
-  String _location;
-  String _address;
+  String logoString;
+  String name;
+  String shortName;
+  String campus;
+  String website;
+  String address;
 
-  List<String> _tags;
+  List<String> tags;
   Hours hours;
-  double _lat;
-  double _lng;
+  double lat;
+  double lng;
+
+  Store({
+    @required this.id,
+    @required this.buildingID,
+    @required this.logoString,
+    @required this.name,
+    @required this.shortName,
+    @required this.campus,
+    @required this.lat,
+    @required this.lng,
+    @required this.website,
+    @required this.address,
+  });
+
+  factory Store.fromJson(Map<String, String> parsedJson) {
+    return Store(
+      id: parsedJson['id'],
+      buildingID: parsedJson['building_id'],
+      name: parsedJson['name'],
+      logoString: parsedJson['logo'],
+      shortName: parsedJson['short_name'],
+      campus: parsedJson['campus'],
+      lat: double.parse(parsedJson['lat']),
+      lng: double.parse(parsedJson['lng']),
+      address: parsedJson['address'],
+      website: parsedJson['website'],
+    );
+  }
 }
