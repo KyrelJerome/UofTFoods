eb3c80a55788b20031c6faa3906648c4b4f7e7b5
Kyrel Jerome <kyrel2007@gmail.com>
2019-05-29 08:34:08 -0400

    finished StoreFilter?

--- lib/Objects/Filter.dart
+++ lib/Objects/Filter.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'Store.dart';
+
 class StoreFilter {
   String _label;
   String _toolTip;
   Function(bool) _action;
-  Function _filter;
-  bool state;
+  Function(Store) _filter;
+  bool _state;// output the filter chip's state
 
-  bool isFiltered(){
-    return _filter();
+  bool isFiltered(Store store){
+    _state = _filter(store);
+    return _state;
   }
-  StoreFilter({_action, _filter: filter, })
+  StoreFilter(Function(bool) action, String tooltip, String label, bool state, Function(Store) filter){
+    _action = action;
+    _filter = filter;
+    _toolTip = tooltip;
+    _label = label;
+    _state = state;
+  }
+
   get filterChip {
     return FilterChip(
       tooltip: _toolTip ,
-      selected: state,
+      selected: _state,
       label: Text(_label),
       onSelected: _action,
     );
