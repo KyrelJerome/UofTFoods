55ee4106c63bae0d7876e0ed39cf722d53d13a8c
Kyrel Jerome <kyrel2007@gmail.com>
2019-06-04 17:29:06 -0400

    Added credits screen

---
+++ lib/StoreUI/TimeBasedNote.dart
@@ -0,0 +1,12 @@
+import 'package:deer_food/Objects/Store.dart';
+import 'package:flutter/material.dart';
+
+class TimeBasedNote extends StatelessWidget {
+  final Store store;
+
+  const TimeBasedNote({Key key, this.store}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+  
+  }
+}
